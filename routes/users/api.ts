934c3115c20acad838d205833a7ab189a3aa9693
Hafiidh Luqmanul Hakim <hafidluqmanulhakim@gmail.com>
2024-07-12 23:28:06 +0800

    feat: update variable name api.ts (#1)

--- routes/users/api.ts
+++ routes/users/api.ts
@@ -2,13 +2,13 @@ import express, { Express, Request, Response } from 'express'
 import { none } from '@config/multer'
 import { userController } from '@Controllers/User'
 
-const usersRoute: Express = express()
+const userAuthRoute: Express = express()
 
-usersRoute.route('/')
+userAuthRoute.route('/')
   .get(none.none(), (req: Request, res: Response): Promise<void> => userController.getAll(req, res))
   .post(none.none(), (req: Request, res: Response): Promise<void> => userController.insert(req, res))
 
-usersRoute.route('/:id')
+userAuthRoute.route('/:id')
   .get(none.none(), (req: Request, res: Response): Promise<void> => userController.getById(req, res))
   .put(none.none(), (req: Request, res: Response): Promise<void> => userController.update(req, res))
   .delete(none.none(), (req: Request, res: Response): Promise<void> => userController.delete(req, res))
